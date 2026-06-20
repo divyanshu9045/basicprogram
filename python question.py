988f8f61cfdef0a5b6df03fc02e46c97e41cc97c
divyanshu9045 <160527236+divyanshu9045@users.noreply.github.com>
2026-06-20 12:47:47 +0530

    Update python question.py

--- python question.py
+++ python question.py
@@ -71,6 +71,74 @@ else:
     print("balance is below the minimum limit")
 
 
+#11.Check whether a number is even or odd
+n=int(input("enter a number:"))
+if(n%2==0):
+    print("Number is even ")
+else:
+    print("number is odd ")
 
+#12. Find the largest of two numbers.
+a=float(input("enter a number:"))
+b=float(input("enter a number:"))
+if(a>b):
+    print("a is gretest number")
+else:
+    print("b is greates number")
+
+#13. Check whether a person is eligible for driving license.
+age=float(input("enter your age :"))
+if(age>=18):
+    print(" person is eligible for driving license")
+else:
+    print("person is not elgible for driving license")
+
+#14. Print Pass or Fail based on marks.
+marks=float(input("enter your marks"))
+if(marks>=45):
+            print("pass")
+else:
+    print("fail")
+
+#15. Check whether a number is positive or negative.
+a=int(input("enter a number: "))
+if(a>0):
+    print("number is postive")
+else:
+    print("number is negative")
+    
+#16. Check whether a character is a vowel or consonant.
+ab=(input("enter a character:"))
+if(ab in("aeiouAEIOU")):
+    print("vowels")
+else:
+    print("consonant")
+#17. Check if a year is leap or not.
+year=int(input("enter a year :"))
+if(year%4==0):
+    print("this is leap year")
+else:
+    print("this is not a leap year")
+    #18. Print Valid Password or Invalid Password
+p=(input("enter a password:"))
+if(p=="div123"):
+    print("valid password ")
+else:
+    print("invalid")
+#19. Determine whether salary is taxable or not.
+
+s=float(input("enter the salary:"))
+if(s>=8000000):
+    print(" salary is taxable")
+else:
+    Print("salary is not taxable")
+    
+#20. Check whether a number is greater than 50 or not.
+
+n=float(input("enter a number:"))
+if(n>50):
+    print("number is greater then 50")
+else:
+    print("number is less then 50")
     
 
