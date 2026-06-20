a956dcdc117fa3d86ce41582b8adb0d6a84d2c11
divyanshu9045 <160527236+divyanshu9045@users.noreply.github.com>
2026-06-20 11:45:02 +0530

    Update python question.py

--- python question.py
+++ python question.py
@@ -1,21 +1,18 @@
-""""
+
 # Write a Python program to check if a number is positive.
 a=int(input("enter a number: "))
 if(a>0):
     print("number is postive")
 else:
     print("number is negative")
-    """
-""""
+    "
 #2. Print "Eligible to vote" if age is 18 or above.
 age=int(input("enter the age:"))
 if(age>=18):
     print("elgible for vote")
 else:
     print("not elgible for vote")
-"""
 
-"""
 #3. Check if a number is divisible by 7.
 a=int(input("enter a number :"))
 if(a%7==0):
@@ -23,17 +20,14 @@ if(a%7==0):
 else:
     print(" no is  not divisble by 7")
 
-"""
-"""
+
 #4. Print "Pass" if marks are greater than 40
 marks=float(input("enter marks :"))
 if(marks>40):
     print("pass")
 else:
     print("false")
-    """
-
-""""
+ 
 #5. Check if a number is greater than 100.
 a=float(input("enter a number:"))
 if (a>100):
@@ -41,32 +35,26 @@ if (a>100):
 else:
     print("number is less then 100")
 
-    """
-
-"""
+    
 #6. Display a message if temperature exceeds 45°C.
 
 temp=float(input("enter the temperatuere:"))
 if(temp>45):
     print("temperature is exceed from 45 degree")
-"""
-"""
+    
 #7. Check if a string length is more than 8 characters.
 name=(input("enetr string :"))
 if(len(name)>8):
     print(" string ")
-"""
-"""
+
 #8. Check if a number is a multiple of 10.
 a=float(input("enter the number:"))
 if(a%10==0):
     print("number is multiple of 10")
 else:
     print("number is not multiple of 10")
-    """
-
-"""
-9 Print "Logged In" if password matches "admin123".
+    
+#9 Print "Logged In" if password matches "admin123".
 
 
 password=(input("enter password :"))
@@ -74,8 +62,8 @@ if(password=="admin123"):
     print("logged in")
 else:
     print("not loggin")
-    """
-"10. Print a warning if balance is below minimum limit."
+
+1#0. Print a warning if balance is below minimum limit.
 balance=float(input("enter amount:"))
 if(balance>1000):
     print(balance)
